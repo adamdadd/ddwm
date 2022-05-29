9ec5f6050aa5c948f55e93c41d38b65a84974bac
adamdad <adam-dad@hotmail.co.uk>
2022-05-29 14:15:23 +0100

    steam hotkey

--- config.h
+++ config.h
@@ -29,6 +29,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       1 << 8,       1,           -1 },
 	{ "firefox",  NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "Steam",    NULL,       NULL,       1 << 2,       0,           -1 },
 };
 
 /* layout(s) */
