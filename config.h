31239e26d5839d7ad0acb8e7b492499f112296aa
adamdad <adam-dad@hotmail.co.uk>
2022-05-29 14:41:05 +0100

    full gaps patch

--- config.h
+++ config.h
@@ -2,6 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int gappx     = 5;        /* pixel gap around windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -101,6 +102,9 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
+	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
+        { MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
+        { MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
 	{ MODKEY|ControlMask|ShiftMask,             XK_q,      quit,           {0} },
 };
 
