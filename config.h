09ec1281eb578381f3da6e3baa794689e312175a
adamdad <adam-dad@hotmail.co.uk>
2022-12-30 03:41:29 +0000

    change colour of active window

--- config.h
+++ config.h
@@ -16,7 +16,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char focus_font[]      = "#fbf1c7";
-static const char col_focus[]       = "#d65d0e";
+static const char col_focus[]       = "#928374";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
